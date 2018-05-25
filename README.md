f80c520aa3020c7355de4aaf6fa601b5a84fb3f2
shubh3110 <xarvis31102347@gmail.com>
2018-05-25 15:54:48 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Scraper
+
+It scraps RegNo ,Name and Cgpa of undergrad Computer Science students at NIT JAMSHEDPUR.
+It calculates rank of each student according to CGPA by using "sort_values()" method of Pandas Dataframe according to specific column no and finally stores result into csv file.
+
+### Packages
+It uses Selenium,Pandas,Beautiful Soup for scraping website.
---
+++ scrap.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import bs4
+import csv
+import pandas as pd
+
+driver = webdriver.Firefox()
+driver.get('http://14.139.205.172/web_new/Default.aspx')
+
+with open('result.csv', 'w') as f:
+    f.write("RegId, Name ,CGPA \n")
+
+#NA = [8,13,58,69,72,86]
+
+for i in range(1,93):
+	if i !=8 and i != 13 and i != 58 and i != 69 and i != 72 and i != 86:
+		roll = str(i)
+		if i<10:
+			regno = '2015UGCS00' + roll
+		else: 
+			regno = '2015UGCS0' + roll
+		
+		driver.find_element_by_name('txtRegno').clear()
+		driver.find_element_by_name('txtRegno').send_keys(regno)
+		driver.find_element_by_name('btnimgShow').click()
+		sem = Select(driver.find_element_by_name('ddlSemester'))
+		sem.select_by_value('6')
+		showresult = driver.find_element_by_name('btnimgShowResult')
+		showresult.click()
+		res = driver.page_source
+		soup = bs4.BeautifulSoup(res,'lxml')
+		cg = soup.select('#lblCPI')
+		cg = float(cg[0].text)
+		name = soup.select('#lblSName')
+		name = str(name[0].text)
+
+		with open('result.csv', 'a') as f:
+			f.write(regno+","+name+","+cg+"\n")
+
+df=pd.read_csv('/home/shubham/Desktop/res.csv')
+df=df.sort_values('2')
+df.insert(0, 'Rank', range(1,1+len(df)))
+
+df.to_csv('result.csv', sep='\t', encoding='utf-8')
+
+
+
+
+	
