339ce683594ff865d418111e8d5ff48b043793fc
shubh3110 <xarvis31102347@gmail.com>
2018-05-31 03:01:06 +0530

    initial commit

--- scrap.py
+++ scrap.py
@@ -9,24 +9,24 @@ import numpy as np
 driver = webdriver.Firefox()
 driver.get('http://14.139.205.172/web_new/Default.aspx')
 
-with open('mcaresult.csv', 'w') as f:
+with open('result.csv', 'w') as f:
     f.write("RegId" + "," + "Name" + "," + "CGPA" + "\n")
 
 #NA = [8,13,58,69,72,86]
 
-for i in range(1,79):
-	if i != 23 and i!=47 and i!=56 and i!=66:
+for i in range(1,93):
+	if i != 8 and i!=13 and i!=58 and i!=69 and i!=72 and i!=86:
 		roll = str(i)
 		if i<10:
-			regno = '2017PGCACA0' + roll
+			regno = '2015UGCS00' + roll
 		else: 
-			regno = '2017PGCACA' + roll
+			regno = '2015UGCS0' + roll
 		
 		driver.find_element_by_name('txtRegno').clear()
 		driver.find_element_by_name('txtRegno').send_keys(regno)
 		driver.find_element_by_name('btnimgShow').click()
 		sem = Select(driver.find_element_by_name('ddlSemester'))
-		sem.select_by_value('2')
+		sem.select_by_value('6')
 		showresult = driver.find_element_by_name('btnimgShowResult')
 		showresult.click()
 		res = driver.page_source
@@ -37,17 +37,11 @@ for i in range(1,79):
 		name = soup.select('#lblSName')
 		name = str(name[0].text)
 
-		with open('mcaresult.csv', 'a') as f:
+		with open('result.csv', 'a') as f:
 			f.write(regno+","+name+","+cg+"\n")
 
 
-df=pd.read_csv('mcaresult.csv')
+df=pd.read_csv('result.csv')
 df=df.sort_values('CGPA',ascending=[False])
 df.insert(0, 'Rank', range(1,1+len(df)))
-df.head(15)
-df.to_csv('mcaoutput.csv', encoding='utf-8', index=False)
-
-
-
-
-	
+df.to_csv('output.csv', encoding='utf-8', index=False)
