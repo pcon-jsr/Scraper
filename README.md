0fc55e2951408020e1a0c63e3a6d8d121d9b8eb9
shubh3110 <xarvis31102347@gmail.com>
2018-05-31 01:19:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ It scraps RegNo ,Name and Cgpa of undergrad Computer Science students at NIT JAM
 It calculates rank of each student according to CGPA by using "sort_values()" method of Pandas Dataframe according to specific column no and finally stores result into csv file.
 
 ### Packages
-It uses Selenium,Pandas,Beautiful Soup for scraping website.
+Selenium,Pandas,Beautiful Soup.
--- scrap.py
+++ scrap.py
@@ -4,12 +4,13 @@ from selenium.webdriver.support.ui import Select
 import bs4
 import csv
 import pandas as pd
+import numpy as np
 
 driver = webdriver.Firefox()
 driver.get('http://14.139.205.172/web_new/Default.aspx')
 
 with open('result.csv', 'w') as f:
-    f.write("RegId, Name ,CGPA \n")
+    f.write("RegId" + "," + "Name" + "," + "CGPA" + "\n")
 
 #NA = [8,13,58,69,72,86]
 
@@ -32,17 +33,19 @@ for i in range(1,93):
 		soup = bs4.BeautifulSoup(res,'lxml')
 		cg = soup.select('#lblCPI')
 		cg = float(cg[0].text)
+		cg=str(cg)
 		name = soup.select('#lblSName')
 		name = str(name[0].text)
 
 		with open('result.csv', 'a') as f:
 			f.write(regno+","+name+","+cg+"\n")
 
-df=pd.read_csv('/home/shubham/Desktop/res.csv')
-df=df.sort_values('2')
-df.insert(0, 'Rank', range(1,1+len(df)))
 
-df.to_csv('result.csv', sep='\t', encoding='utf-8')
+df=pd.read_csv('result.csv')
+df=df.sort_values('CGPA',ascending=[False])
+df.insert(0, 'Rank', range(1,1+len(df)))
+df.head(15)
+df.to_csv('output.csv', encoding='utf-8', index=False)
 
 
 
