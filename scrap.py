7fb2885b1b6d80af39624a9db70e7b4a3468664f
shubh3110 <xarvis31102347@gmail.com>
2018-05-31 21:26:53 +0530

    initial commit

--- scrap.py
+++ scrap.py
@@ -1,10 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException
 import bs4
 import csv
 import pandas as pd
 import numpy as np
+
+def is_alert_present():
+    try: driver.switch_to_alert().accept()
+    except NoAlertPresentException, e: return False
+    return True
 
 driver = webdriver.Firefox()
 driver.get('http://14.139.205.172/web_new/Default.aspx')
@@ -12,34 +20,36 @@ driver.get('http://14.139.205.172/web_new/Default.aspx')
 with open('result.csv', 'w') as f:
     f.write("RegId" + "," + "Name" + "," + "CGPA" + "\n")
 
-#NA = [8,13,58,69,72,86]
-
 for i in range(1,93):
-	if i != 8 and i!=13 and i!=58 and i!=69 and i!=72 and i!=86:
-		roll = str(i)
-		if i<10:
-			regno = '2015UGCS00' + roll
-		else: 
-			regno = '2015UGCS0' + roll
-		
-		driver.find_element_by_name('txtRegno').clear()
-		driver.find_element_by_name('txtRegno').send_keys(regno)
-		driver.find_element_by_name('btnimgShow').click()
-		sem = Select(driver.find_element_by_name('ddlSemester'))
-		sem.select_by_value('6')
-		showresult = driver.find_element_by_name('btnimgShowResult')
-		showresult.click()
-		res = driver.page_source
-		soup = bs4.BeautifulSoup(res,'lxml')
-		cg = soup.select('#lblCPI')
-		cg = float(cg[0].text)
-		cg=str(cg)
-		name = soup.select('#lblSName')
-		name = str(name[0].text)
+    roll = str(i)
+    if i<10:
+        regno = '2015UGCS00' + roll
+    else: 
+        regno = '2015UGCS0' + roll
+    
+    driver.find_element_by_name('txtRegno').clear()
+    driver.find_element_by_name('txtRegno').send_keys(regno)
+    driver.find_element_by_name('btnimgShow').click()
 
-		with open('result.csv', 'a') as f:
-			f.write(regno+","+name+","+cg+"\n")
+    if is_alert_present() == True:
+    	driver.close()
+    	driver = webdriver.Firefox()
+    	driver.get('http://14.139.205.172/web_new/Default.aspx')
+    else:
+        sem = Select(driver.find_element_by_name('ddlSemester'))
+        sem.select_by_value('6')
+        showresult = driver.find_element_by_name('btnimgShowResult')
+        showresult.click()
+        res = driver.page_source
+        soup = bs4.BeautifulSoup(res,'lxml')
+        name = soup.select('#lblSName')
+        name = str(name[0].text)
+        cg = soup.select('#lblCPI')
+        cg = float(cg[0].text)
+        cg=str(cg)
 
+        with open('result.csv', 'a') as f:
+            f.write(regno+","+name+","+cg+"\n")
 
 df=pd.read_csv('result.csv')
 df=df.sort_values('CGPA',ascending=[False])
